9b7568e1a4f981e5c33890a53837731d88dfb4a0
harpreet <harpreetbenipal95@gmail.com>
2019-05-26 18:46:12 -0700

    Test

--- flightdelays.sh
+++ flightdelays.sh
@@ -3,4 +3,4 @@
 csvcut -c ArrDelay,Origin flightdelays.csv | csvgrep -c Origin -m "SFO" | head -4 > first3sfo.csv
 csvlook first3sfo.csv
 
-
+echo "harpreet"
--- flightdelays1.sh
+++ flightdelays1.sh
@@ -4,3 +4,5 @@ cut -d',' -f18 flightdelays.csv | sort | uniq -c | sort -r -n | head -3 | cut -d
 echo "No_of_Arriving_Planes Destination_Airport" > headerdest.csv
 cat headerdest.csv top3dest.csv > combinedtopdest.csv
 csvlook combinedtopdest.csv
+
+echo "harpreet"
